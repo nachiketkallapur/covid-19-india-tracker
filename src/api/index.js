09b833730eb81e9c0cea0155a5b7aabaad3472b6
nachiketkallapur <nachiketkallapur@gmail.com>
2020-05-11 23:21:31 +0530

    Added Card component

--- src/api/index.js
+++ src/api/index.js
@@ -6,13 +6,15 @@ const dailyDataUrl = "https://api.rootnet.in/covid19-in/stats/history";
 export const fetchData = async () => {
     const response = await axios.get(totalDataUrl);
     const { total, deaths, discharged, confirmedCasesIndian, confirmedCasesForeign } = response.data.data.summary;
-    
+    const lastUpdate = response.data.lastOriginUpdate;
+    console.log(new Date(lastUpdate).toDateString());
     return {
         infected: total,
         deaths: deaths,
         recovered: discharged,
         casesIndian: confirmedCasesIndian,
-        casesForeign: confirmedCasesForeign
+        casesForeign: confirmedCasesForeign,
+        lastUpdate
     };
 
 }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,24 +3,66 @@ import React, { useEffect, useState } from 'react';
 import './card.styles.scss';
 import { fetchData } from '../../api';
 
+import { Grid, CardContent, Typography } from '@material-ui/core';
+import CountUp from 'react-countup';
+
 
 const Card = () => {
-    const [covidData, setCovidData] = useState({});
+    const [covidData, setCovidData] = useState(null);
 
-    useEffect(()=>{
-        const fetchFunction = async () =>{
+    useEffect(() => {
+        const fetchFunction = async () => {
             setCovidData(await fetchData());
         }
+
         fetchFunction();
-    },[])
 
+    }, [])
+
+    if (!covidData) {
+        return (
+            <center>
+                Loading...
+            </center>
+        )
+    }
+
+    const { infected, recovered, deaths, lastUpdate } = covidData;
     
-
-    console.log(covidData.infected);
     return (
-        <h2>Card</h2>
+        <Grid container className='grid-container'>
+            <Grid item className='infected'>
+                <CardContent>
+                    <Typography><b>Infected</b></Typography>
+                    <Typography>
+                        <CountUp start={0} end={infected} duration={1.5} separator=',' />
+                    </Typography>
+                    <Typography>{new Date(lastUpdate).toDateString()}</Typography>
+                    <Typography>Number of affected from COVID-19</Typography>
+                </CardContent>
+            </Grid>
+            <Grid item className='recovered'>
+                <CardContent>
+                    <Typography><b>Recovered</b></Typography>
+                    <Typography>
+                        <CountUp start={0} end={recovered} duration={1.5} separator=',' />
+                    </Typography>
+                    <Typography>{new Date(lastUpdate).toDateString()}</Typography>
+                    <Typography>Number of recoveries from COVID-19</Typography>
+                </CardContent>
+            </Grid>
+            <Grid item className='deaths'>
+                <CardContent>
+                    <Typography><b>Deaths</b></Typography>
+                    <Typography>
+                        <CountUp start={0} end={deaths} duration={1.5} separator=',' />
+                    </Typography>
+                    <Typography>{new Date(lastUpdate).toDateString()}</Typography>
+                    <Typography>Number of deaths from COVID-19</Typography>
+                </CardContent>
+            </Grid>
+        </Grid>
     )
 }
 
 export default Card;
-    
