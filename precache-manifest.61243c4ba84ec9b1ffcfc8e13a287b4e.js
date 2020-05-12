1ffde66ffcdf37664bb115b1472f4150b30bf014
nachiketkallapur <nachiketkallapur@gmail.com>
2020-05-13 00:22:11 +0530

    Updates

---
+++ precache-manifest.61243c4ba84ec9b1ffcfc8e13a287b4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf5ee92e31f1bd3dfb7361ced6c23df",
+    "url": "/covid-19-india-tracker/index.html"
+  },
+  {
+    "revision": "241592292dca9abe67b0",
+    "url": "/covid-19-india-tracker/static/css/main.6771a6c1.chunk.css"
+  },
+  {
+    "revision": "41a963a3dc342725fc15",
+    "url": "/covid-19-india-tracker/static/js/2.162949dc.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-19-india-tracker/static/js/2.162949dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241592292dca9abe67b0",
+    "url": "/covid-19-india-tracker/static/js/main.62ce24c4.chunk.js"
+  },
+  {
+    "revision": "e77b98f1bbbe13849828",
+    "url": "/covid-19-india-tracker/static/js/runtime-main.76770231.js"
+  },
+  {
+    "revision": "3dcebebb202bf325d6a47940474aa9f6",
+    "url": "/covid-19-india-tracker/static/media/covid-19-india-tracker(1).3dcebebb.jpg"
+  }
+]);
